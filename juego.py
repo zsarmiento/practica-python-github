95173a498c737c45f0eab55bda130b6fa17563fe
oscar sarmiento <ossarmiento@gmail.com>
2026-06-07 19:21:28 -0500

    nuevos cambios 6 junio juego nuevo

--- juego.py
+++ juego.py
@@ -6,3 +6,8 @@ numero2=int(input("ingresa segundo numero"))
 numerojugador =randint(numero1,numero2)
 
 print(f"numero jugador: {numerojugador} ")
+
+if numerojugador%2==0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
