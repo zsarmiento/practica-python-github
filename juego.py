aafb54e30b81b58ba23785ef05192108083af269
Zulay Sarmiento Arevalo <zulaysarmientoarevalo@gmail.com>
2026-06-09 09:28:00 -0500

    se agregan cambios para notificar que perdio

--- juego.py
+++ juego.py
@@ -1,27 +1,30 @@
 from random import randint
-
-numero1=int(input("ingresa primer numero"))
-numero2=int(input("ingresa segundo numero"))
-numerojugador =randint(numero1,numero2)
-if numerojugador%2==0:
-    print("el numero es par")
-else:
-    print("el numero es impar")
-    numerojugador+=1
-    if numerojugador>numero2:
-        numerojugador-=2
-print("adivina el numero que escogi")
-contador =0
-adivinar = False
-while contador <=2 or adivinar== False:
-    intento=int(input("dame el numero: "))
-    if intento == numerojugador:
-        print("correcto, el numero es acertado")
-        adivinar = True
-        contador =3
+try: 
+        
+    numero1=int(input("ingresa primer numero"))
+    numero2=int(input("ingresa segundo numero"))
+    numerojugador =randint(numero1,numero2)
+    if numerojugador%2==0:
+        print("el numero es par")
     else:
-        if intento > numerojugador:
-            print("el numero ingresado es mayor que el que debes adivinar")
+        print("el numero es impar")
+        numerojugador+=1
+        if numerojugador>numero2:
+            numerojugador-=2
+    print("adivina el numero que escogi")
+    contador =0
+    adivinar = False
+    while contador <=2 and adivinar== False:  
+        intento=int(input("dame el numero: "))
+        if intento == numerojugador:
+            print("correcto, el numero es acertado")
+            adivinar = True
+            contador =3
+        elif contador == 2:
+            print("has perdido")
+            break
         else:
-             print("el numero ingresado es menor que el numero que debes adivinar")
-        contador+=1
+            print("el numero ingresado es mayor que el que debes adivinar") if intento > numerojugador else print("el numero ingresado es menor que el numero que debes adivinar")  
+            contador+=1
+except: 
+    print("el numero ingresado es incorrecto")
